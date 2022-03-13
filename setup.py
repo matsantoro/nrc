dea462e82c804ebf54e76e5a1e20e347eabeb057
matsantoro <matteosantoro561@gmail.com>
2022-03-13 18:04:59 +0100

    Added setup

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version='0.0.1',
     author='Matteo Santoro',
     description='A module for handling reliability and tribe-based computations of experiments on connectomes.',
-    packages=["nrc", "nrc.connectome", "nrc.reliability"],
+    packages=["nrc"],
 )
